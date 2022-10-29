09d059e7df7663c6df592c66fee3d61777ef5e55
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-10-29 16:08:21 +0300

    Only show one notification if two or more were scheduled at the same time ⌛

--- Athan.Avalonia/Services/NotificationService.cs
+++ Athan.Avalonia/Services/NotificationService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using DesktopNotifications;
@@ -11,6 +13,8 @@ namespace Athan.Avalonia.Services;
 internal sealed class NotificationService
 {
     private bool initialized;
+
+    private readonly List<DateTimeOffset> scheduledNotifications = new List<DateTimeOffset>();
 
     private readonly INotificationManager manager;
 
@@ -39,21 +43,38 @@ internal sealed class NotificationService
         if (!initialized)
         {
             await manager.Initialize();
+            manager.NotificationActivated += OnNotificationActivated;
+
             initialized = true;
         }
     }
 
     public async Task ScheduleNotificationAsync(string title, string body, TimeSpan timeSpan)
-    {        
+    {
         if (!initialized)
         {
             throw new InvalidOperationException("The notification manager was not initialized.");
+        }
+
+        var dateTime = DateTimeOffset.Now + timeSpan;
+
+        if (scheduledNotifications.Any(date => date.Hour == dateTime.Hour))
+        {
+            return;
         }
 
         await manager.ScheduleNotification(new Notification()
         {
             Title = title,
             Body = body
-        }, DateTimeOffset.Now + timeSpan);
+        }, dateTime);
+
+        scheduledNotifications.Add(dateTime);
+    }
+
+    private void OnNotificationActivated(object? sender, NotificationActivatedEventArgs eventArgs)
+    {
+        var notification = scheduledNotifications.FirstOrDefault(date => date.Hour == DateTimeOffset.Now.Hour);
+        scheduledNotifications.Remove(notification);
     }
 }
