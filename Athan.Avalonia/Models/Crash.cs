b8f701acb561faa0b8042335c4134105c57880a5
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-11-24 01:23:28 +0300

    Give crashes a more readable and useful messages 💥

---
+++ Athan.Avalonia/Models/Crash.cs
@@ -0,0 +1,3 @@
+﻿namespace Athan.Avalonia.Models;
+
+internal sealed record Crash(DateTime DateTime, string Message);
--- Athan.Avalonia/Program.cs
+++ Athan.Avalonia/Program.cs
@@ -1,4 +1,6 @@
-﻿using Athan.Avalonia;
+﻿using System.Text.Json;
+using Athan.Avalonia;
+using Athan.Avalonia.Models;
 using Avalonia;
 
 try
@@ -13,8 +15,10 @@ catch (Exception exception)
     var directory = Path.Join(App.Directory, "Crashes");
     Directory.CreateDirectory(directory);
 
+    var crash = JsonSerializer.Serialize(new Crash(DateTime.UtcNow, exception.Message));
+
     await File
-        .WriteAllTextAsync(Path.Join(directory, $"{Guid.NewGuid()}.txt"), exception.Message)
+        .WriteAllTextAsync(Path.Join(directory, $"{Guid.NewGuid()}.txt"), crash)
         .ConfigureAwait(false);
 
     Environment.Exit(-1);
