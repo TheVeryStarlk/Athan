168635252094f9dc090d0a04ce2164f2c97971c3
starlkytminecraft@gmail.com <starlkytminecraft@gmail.com>
2022-11-30 01:27:49 +0300

    Fix capitalization 🧢

--- Athan.Avalonia/Languages/Language.Designer.cs
+++ Athan.Avalonia/Languages/Language.Designer.cs
@@ -231,7 +231,7 @@ namespace Athan.Avalonia.Languages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Oh No!.
+        ///   Looks up a localized string similar to Oh no!.
         /// </summary>
         public static string OhNo {
             get {
