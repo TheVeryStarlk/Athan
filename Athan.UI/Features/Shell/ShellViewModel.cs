aa9b021617ae94e6ac07d22e74041c9a12c0540d
Starlk <starlkytminecraft@gmai.com>
2024-12-14 00:43:22 +0300

    Change title.

--- Athan.UI/Features/Shell/ShellViewModel.cs
+++ Athan.UI/Features/Shell/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace Athan.UI.Features.Shell;
@@ -8,9 +9,18 @@ internal sealed partial class ShellViewModel : ObservableObject
     [ObservableProperty]
     public partial string Message { get; set; } = "Greetings!";
 
+    private int count;
+
     [RelayCommand]
     private void Greet()
     {
-        Message = "Hello!";
+        count++;
+
+        var result = new StringBuilder("Hell")
+            .Append(new string('o', count))
+            .Append('!')
+            .ToString();
+
+        Message = result;
     }
 }
