47dcce9402942b4ec1cc4450c1124b01603f9bae
Starlk <starlkytminecraft@gmai.com>
2024-12-14 03:45:00 +0300

    Create a splash view.

---
+++ Athan.UI/Features/Shell/SplashView.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Athan.UI.Features.Shell;
+
+internal sealed partial class SplashView : Page
+{
+    public SplashView()
+    {
+        InitializeComponent();
+    }
+}
