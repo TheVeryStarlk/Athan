4b3a30870e569d18588fb15eec3a57d7eeed2751
Starlk <starlkytminecraft@gmai.com>
2024-12-15 02:55:00 +0300

    Simple implementation for single instance. Should consider to do it the proper way instead.

--- Athan.UI/App.xaml.cs
+++ Athan.UI/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Athan.UI.Features.Shell;
+﻿using System.Reflection;
+using Athan.UI.Features.Shell;
 using Athan.UI.Features.Welcome;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +17,16 @@ public sealed partial class App : Application
 
     protected override void OnLaunched(LaunchActivatedEventArgs eventArgs)
     {
+        const string name = ".lock";
+
+        if (File.Exists(name))
+        {
+            File.WriteAllBytes(name, [byte.MaxValue]);
+            Exit();
+
+            return;
+        }
+
         var services = Bootstrapper.Create();
 
         var window = new Window
@@ -25,9 +36,27 @@ public sealed partial class App : Application
             SystemBackdrop = new MicaBackdrop()
         };
 
-        window.Activate();
-
         var request = new NavigationRequest(services.GetRequiredService<WelcomeViewModel>());
         WeakReferenceMessenger.Default.Send(request);
+
+        window.Activate();
+
+        File.WriteAllBytes(name, []);
+
+        var watcher = new FileSystemWatcher(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+            "*.lock")
+        {
+            NotifyFilter = NotifyFilters.LastWrite,
+            EnableRaisingEvents = true
+        };
+
+        watcher.Changed += (_, _) => window.Show();
+
+        window.Closed += (_, _) =>
+        {
+            File.Delete(name);
+            watcher.Dispose();
+        };
     }
 }
