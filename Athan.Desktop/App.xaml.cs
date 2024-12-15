8c3c6bd318c3fa261a0febed8877b3901ab24029
Starlk <starlkytminecraft@gmai.com>
2024-12-15 22:56:23 +0300

    Initial commit. (With DI and CT)

---
+++ Athan.Desktop/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Athan.Desktop;
+
+public sealed partial class App
+{
+    private readonly IServiceProvider services = new ServiceCollection()
+        .AddTransient<ShellView>()
+        .BuildServiceProvider();
+
+    protected override void OnStartup(StartupEventArgs eventArgs)
+    {
+        base.OnStartup(eventArgs);
+
+        MainWindow = services.GetRequiredService<ShellView>();
+        MainWindow.Show();
+    }
+}
---
+++ Athan.Desktop/ShellView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Wpf.Ui.Appearance;
+
+namespace Athan.Desktop;
+
+public sealed partial class ShellView
+{
+    public ShellView()
+    {
+        InitializeComponent();
+        ApplicationThemeManager.Apply(this);
+    }
+}
