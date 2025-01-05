7014b6255107af7a692889d6314fcc20bf49ae39
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-01-05 04:26:23 +0300

    Add an extension method for matching a task's result.

---
+++ Athan.Avalonia/Extensions/TaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using LightResults;
+
+namespace Athan.Avalonia.Extensions;
+
+internal static class TaskExtensions
+{
+    public static async Task Match<T>(this Task<Result<T>> task, Func<T, Task> success, Action<Result> failure)
+    {
+        try
+        {
+            var result = await task;
+
+            if (!result.IsSuccess(out var value))
+            {
+                failure(Result.Failure(result.Errors));
+            }
+            else
+            {
+                await success(value);
+            }
+        }
+        catch (Exception exception)
+        {
+            failure(Result.Failure(exception.Message));
+        }
+    }
+}
