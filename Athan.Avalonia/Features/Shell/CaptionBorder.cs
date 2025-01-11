84af3f284097607f35a2e0f0a47b8d0d815bb924
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-01-11 21:03:46 +0300

    Change the caption buttons.

---
+++ Athan.Avalonia/Features/Shell/CaptionBorder.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+
+namespace Athan.Avalonia.Features.Shell;
+
+internal sealed class CaptionBorder : Border
+{
+    public CaptionType Type { get; set; }
+
+    protected override void OnPointerEntered(PointerEventArgs eventArgs)
+    {
+        base.OnPointerEntered(eventArgs);
+        Background = Type is CaptionType.Close ? Brushes.Red : Brushes.DimGray;
+    }
+
+    protected override void OnPointerPressed(PointerPressedEventArgs eventArgs)
+    {
+        base.OnPointerPressed(eventArgs);
+        Opacity = 0.75D;
+    }
+
+    protected override void OnPointerReleased(PointerReleasedEventArgs eventArgs)
+    {
+        base.OnPointerReleased(eventArgs);
+        Opacity = 1;
+    }
+
+    protected override void OnPointerExited(PointerEventArgs eventArgs)
+    {
+        base.OnPointerExited(eventArgs);
+        Background = Brushes.Transparent;
+    }
+}
+
+internal enum CaptionType
+{
+    Close,
+    Minimize
+}
--- Athan.Avalonia/Features/Shell/ShellView.axaml.cs
+++ Athan.Avalonia/Features/Shell/ShellView.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Athan.Avalonia.Features.Shell;
 
@@ -8,5 +9,19 @@ internal sealed partial class ShellView : Window
     {
         DataContext = viewModel;
         InitializeComponent();
+    }
+
+    private void CaptionBorderOnPointerReleased(object? sender, PointerReleasedEventArgs eventArgs)
+    {
+        var border = (CaptionBorder) sender!;
+
+        if (border.Type is CaptionType.Close)
+        {
+            Close();
+        }
+        else
+        {
+            WindowState = WindowState.Minimized;
+        }
     }
 }
