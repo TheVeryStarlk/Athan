8b4d14b7d474093a6f4db41077110df8c2bb5a78
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-01-21 21:01:08 +0300

    Show the message on hover.

---
+++ Athan.Avalonia/Features/Prayers/ItemView.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+
+namespace Athan.Avalonia.Features.Prayers;
+
+internal sealed partial class ItemView : UserControl
+{
+    public ItemView()
+    {
+        InitializeComponent();
+    }
+
+    private void ItemOnPointerEntered(object? sender, PointerEventArgs eventArgs)
+    {
+        TimeText.Opacity = 0;
+        MessageText.Opacity = 1;
+    }
+
+    private void ItemOnPointerExited(object? sender, PointerEventArgs eventArgs)
+    {
+        TimeText.Opacity = 1;
+        MessageText.Opacity = 0;
+    }
+}
