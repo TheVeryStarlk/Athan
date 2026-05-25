c0ef1c50ce3d66ccb411b2af8ff2bd288ba3d02c
TheVeryStarlk <45287860+TheVeryStarlk@users.noreply.github.com>
2026-05-25 06:13:01 +0300

    Update selected prayer row

---
+++ Siraj/Features/Prayers/UpcomingToMarginConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Siraj.Features.Prayers.Calculation;
+using System;
+
+namespace Siraj.Features.Prayers;
+
+internal sealed partial class UpcomingToRowConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return (int) (PrayerKind) value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new InvalidOperationException();
+    }
+}
